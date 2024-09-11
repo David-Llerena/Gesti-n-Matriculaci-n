2930def7a8b44156689e9c201fd56c5627a65e5e
David-Llerena <dllerena@espol.edu.ec>
2024-09-11 12:02:08 -0500

    Se agrega botonera con referencias

---
+++ index.js
@@ -0,0 +1,10 @@
+
+redirigirAulas=function(){
+    window.location.assign ("gestionAulas_Anahi_Rodas.html");
+}
+redirigirEstudiantes=function(){
+    window.location.assign ("gestionEstudiante_davidLlerena.html") ;
+}
+redirigirProfesores=function(){
+    window.location.assign ("gestionProfesores_HenryAlvarez.html");
+}
