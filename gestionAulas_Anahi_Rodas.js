a4c58946666f1b96f478f6a806da5162bef8a4cb
CaritoRodas <anahirodas07a@gmail.com>
2024-09-11 11:46:40 -0500

    Prueba Grupal, Anahi Rodas Gestión de Aulas

---
+++ gestionAulas_Anahi_Rodas.js
@@ -0,0 +1,58 @@
+let aulas = [];
+
+crearAula=function(){
+    let numAulta=recuperarTexto("txtNumeroAula");
+    let horario=recuperarTexto("txtHorario");
+    let capMaxima = recuperarTexto("txtCapacidadMaxima");
+    let aula = {}
+    aula.numaula = numAulta;
+    aula.horario = horario;
+    aula.capacidadMaxima = capMaxima;
+    return aula;
+}
+
+agregarAula=function(){
+    let aula = crearAula();
+    let elemtentoAula= aulas.push(aula);
+    console.log("Aulas: "+elemtentoAula);
+
+}
+
+refrescarAula=function(){
+    let cmptabla=document.getElementById("tablaAula");
+    let tabla= "<table> <tr>"+ "<th>Numero Aula </th> "+ "<th>Horario </th> "+"<th>Capacidad Maxima </th> "+"</tr>" 
+    let elmentoTabla;
+    for(let i=0; i<aulas.length; i++){
+        elmentoTabla=aulas[i];
+        tabla+="<tr>"+ "<td>"+elmentoTabla.numaula +"</td> "+ "<td>"+elmentoTabla.horario+" </td> "+"<td>"+elmentoTabla.capacidadMaxima+" </td> "+"</tr>"
+
+    }
+    tabla+="</table>"
+    cmptabla.innerHTML=tabla;
+}
+
+buscar=function(numeroAula){
+    
+    let aulaEncontrada = null;
+    let elemtentoAula;
+    for(let i = 0; i< aulas.length; i++){
+        elemtentoAula=aulas[i];
+        if(elemtentoAula.numaula== numeroAula){
+            aulaEncontrada=elemtentoAula;
+            break;
+        }
+
+    }
+    return aulaEncontrada
+}
+
+buscarAula=function(){
+    let numeroAula=recuperarTexto("txtBuscar");
+    let aulaEncontrada = buscar(numeroAula);
+    if(aulaEncontrada!= null){
+        alert("Aula Encontrada")
+
+    }else{
+        alert("Aula No Encontrada")
+    }
+}
